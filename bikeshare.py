0432c8d366688c15c16d3be13f29d56ac402371b
Rahaf ali <Rahaf_alhazmi@hotmail.com>
2021-03-09 20:21:07 +0300

    SECTION 3
    write goodbye message on python file

--- bikeshare.py
+++ bikeshare.py
@@ -232,6 +232,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+		    print("Thank you for exploring bikeshare data GOODBYE !") 
             break
 
 
